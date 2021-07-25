f35ebc91ad1297acc1bca7b228ec7dc2272d937d
Hector Manuel Chavez Troncoso <hectorchavezt@hotmail.com>
2021-07-24 21:10:53 -0500

    Componentes Universidad, Materia y Facultad
    Creación de los componentes para las vistas de Universidad, Materia y Facultad.

---
+++ CalificameApp/app/src/main/java/com/esielkar/calificame/recov_pass.kt
@@ -0,0 +1,11 @@
+package com.esielkar.calificame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recov_pass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recov_pass)
+    }
+}
