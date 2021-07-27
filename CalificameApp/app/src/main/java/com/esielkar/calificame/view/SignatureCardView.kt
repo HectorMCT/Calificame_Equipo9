5c2437a65ba7d6d458ba3a97394662f661c1f939
Esiel Arizmendi <esiel_kar@hotmail.com>
2021-07-27 14:43:16 -0500

    From Subject to Signature
    Se cambio el nombre de la clase SubjectCardView por SignatureCardView, así como los respectivos cambios en XML para hacer el refactor.
    
    Adicionalmente se creo su propio archivo XML para sus atributos, separandolos así del archivo general attrs.

---
+++ CalificameApp/app/src/main/java/com/esielkar/calificame/view/SignatureCardView.kt
@@ -0,0 +1,30 @@
+package com.esielkar.calificame.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.esielkar.calificame.R
+import com.google.android.material.card.MaterialCardView
+
+class SignatureCardView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : MaterialCardView(context, attrs, defStyleAttr) {
+    private val subjectNameTextView : TextView
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.signature_card_view, this, true)
+        subjectNameTextView = findViewById(R.id.subject_name)
+
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.SignatureCardView, defStyleAttr, 0)
+            val subjectName = typedArray.getText(R.styleable.SignatureCardView_signature_name) ?: resources.getText(R.string.not_defined_label)
+            this.subjectName = subjectName
+            typedArray.recycle()
+        }
+    }
+
+    var subjectName : CharSequence
+    set(value) {subjectNameTextView.text = value}
+    get() = subjectNameTextView.text
+}
