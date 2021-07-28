b07028c695c9768a27bead8dd55045797b05be65
Esiel Arizmendi <esiel_kar@hotmail.com>
2021-07-28 00:58:10 -0500

    Eliminación de importaciones innecesarias
    Se eliminan las importaciones innecesarias

--- CalificameApp/app/src/main/java/com/esielkar/calificame/AboutUsActivity.kt
+++ CalificameApp/app/src/main/java/com/esielkar/calificame/AboutUsActivity.kt
@@ -2,7 +2,6 @@ package com.esielkar.calificame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.esielkar.calificame.R
 
 class AboutUsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
